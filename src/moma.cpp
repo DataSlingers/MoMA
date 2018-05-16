1ab699146f01c0c6b17a0f3b18a7809493979431
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-16 15:15:57 +1000

    Try to build class
    - Pass build in R studio

--- src/moma.cpp
+++ src/moma.cpp
@@ -1,8 +1,14 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 #include "RcppArmadillo.h"
 
-
 // [[Rcpp::export]]
 int doubleMe(const int & x) {
+
     return x+x;
 }
+
+class Prox{
+private:
+    double gamma;
+
+};
