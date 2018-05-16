e9d0f0daa75f615128d3d7e276380bc3912f3c1d
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-16 18:49:32 +1000

    Good

--- src/moma.cpp
+++ src/moma.cpp
@@ -6,23 +6,21 @@ using namespace arma;
 
 class Prox{
 public:
-    virtual arma::vec prox(arma::vec x, double l);
+    virtual arma::vec thres(arma::vec x, double l);
 };
 
 class Lasso: public Prox{
 public:
-    arma::vec prox(arma::vec x, double l){
+    arma::vec thres(arma::vec x, double l){
         printf("In Lasso.prox");
         return sign(x) % max(abs(x) - l, zeros(size(x)));
     }
 };
 
 // [[Rcpp::export]]
-
-arma::vec Lasso(arma::vec x, double l)
+arma::vec test(arma::vec x, double l)
 {
-    class Lasso a;
+    Lasso a;
 
-    cout << "lassoing" << endl;
-    return sign(x) % max(abs(x) - l, zeros(size(x)));
-};
+    return x;
+}
