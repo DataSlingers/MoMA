824d9497d54e8b1fc40f5c9edcd801842a946987
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-16 16:43:03 +1000

    Good
    -pass build
    -good Lasso()

--- src/moma.cpp
+++ src/moma.cpp
@@ -1,14 +1,26 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 #include "RcppArmadillo.h"
 
-// [[Rcpp::export]]
-int doubleMe(const int & x) {
+using namespace arma;
 
-    return x+x;
-}
 
 class Prox{
-private:
-    double gamma;
+public:
+    virtual arma::vec prox(arma::vec x, double l);
+};
 
+class Lasso: public Prox{
+public:
+    arma::vec prox(arma::vec x, double l){
+        printf("In Lasso.prox");
+        return sign(x) % max(abs(x) - l, zeros(size(x)));
+    }
+};
+
+// [[Rcpp::export]]
+
+arma::vec Lasso(arma::vec x, double l)
+{
+    cout << "lassoing" << endl;
+    return sign(x) % max(abs(x) - l, zeros(size(x)));
 };
