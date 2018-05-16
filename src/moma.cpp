bfb4177b8f146f86173710f0163a300cf6cf166c
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-16 16:13:39 +1000

    BAD

--- src/moma.cpp
+++ src/moma.cpp
@@ -1,14 +1,44 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 #include "RcppArmadillo.h"
 
+bool DEBUG = TRUE;
+using namespace arma;
+
+
+
 // [[Rcpp::export]]
 int doubleMe(const int & x) {
-
     return x+x;
 }
 
-class Prox{
-private:
-    double gamma;
 
+// [[Rcpp::export]]
+arma::vec soft_thres(arma::vec x, double l)
+{
+    return sign(x) % max(abs(x) - l, zeros(size(x)));
 };
+
+
+
+
+class Prox{ 
+public:
+    virtual vec prox(vec x,double l);
+};
+
+class Lasso: public Prox{
+private:
+public:
+    vec prox(vec x,double l){
+        return sign(x) % max(abs(x) - l, zeros(size(x)));
+    }
+};
+
+
+
+// [[Rcpp::export]]
+arma::vec test(arma::vec x,double l){
+    Prox *a = new Lasso;
+    return a->prox(x,l);
+}
+
