9faf79dafe28dd4bebedba76f1c8c3b93c2e2a32
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-16 16:25:40 +1000

    OK
    -pass build

--- src/moma.cpp
+++ src/moma.cpp
@@ -1,21 +1,14 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 #include "RcppArmadillo.h"
 
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of bfb4177... BAD
 // [[Rcpp::export]]
 int doubleMe(const int & x) {
 
     return x+x;
 }
-<<<<<<< HEAD
-=======
 
 class Prox{
 private:
     double gamma;
 
 };
->>>>>>> parent of bfb4177... BAD
