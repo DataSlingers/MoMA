1e69d53436c512d87062f9c576a0e702d5ecdf02
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-17 20:51:42 +1000

    Add scad
    - pass build
    - verify results of scad

--- src/moma.cpp
+++ src/moma.cpp
@@ -1,11 +1,12 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 #include "RcppArmadillo.h"
 #include "moma.h"
+using namespace Rcpp;
 using namespace arma;
 #define MAX(a,b) (a)>(b)?(a):(b)
 
 
- class Prox{
+class Prox{
 public:
     virtual arma::vec prox(const arma::vec &x, double l)=0;
 };
@@ -13,17 +14,20 @@ public:
 class Lasso: public Prox{
 public:
     arma::vec prox(const arma::vec &x, double l){
-        Rcpp::Rcout<<("In Lasso.prox\n");
         return sign(x) % arma::max(abs(x) - l, zeros(size(x)));
     }
 };
 
 class Scad: public Prox{
 private:
-    double gamma;
+    double gamma; // >= 2
     
 public:
-    Scad(double g=3.7){gamma=g;}
+    Scad(double g=3.7){
+        if(g<2) 
+        Rcpp::stop("Gamma for MCP should be larger than 2!\n");
+        gamma=g;
+    }
     arma::vec prox(const arma::vec &x, double l){
         int n = x.n_elem;
         arma::vec z(n);
@@ -39,11 +43,45 @@ public:
     }
 };
 
+
+class Mcp: public Prox{
+private:
+    double gamma; // >= 1
+    
+public:
+    Mcp(double g=3.7){
+        if(g<1) stop("Gamma for MCP should be larger than 2!\n");
+        gamma=g;}
+    arma::vec prox(const arma::vec &x, double l){
+        int n = x.n_elem;
+        arma::vec z(n);
+        arma::vec abs = arma::abs(x);
+        arma::vec sgn = sign(x);
+        for (int i = 0; i < n; i++) // Probably need vectorization
+        {
+// this implementation follows ariable Selection via Nonconcave Penalized Likelihood and its Oracle Properties
+// Jianqing Fan and Runze Li, formula(2.8)
+            z(i) = abs(i) > gamma * l ? x(i)
+                                    : (abs(i) > 2 * l ? ((gamma - 1) * x(i) - sgn(i) * gamma * l) / (gamma - 2)
+                                                        : sgn(i) * MAX(double(abs(i) - l),0.0));
+        }
+        return z;    
+    }
+};
+
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-arma::vec test(arma::vec x, double l)
+arma::vec test_lasso(arma::vec x, double l)
 {
     Lasso a;
-    Rcpp::Rcout<< "In Test\n"<< std::endl;
-    return a.prox(x,l);;
+    return a.prox(x,l);
+};
+
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+arma::vec test_scad(arma::vec x, double l,double g=3.7)
+{
+    Scad a(g);
+    return a.prox(x,l);
 };
