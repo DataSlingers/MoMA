def9ef2552586513c350c8030ce2dd880d0f9178
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-17 21:51:49 +1000

    Nasty bug
    line 71-77

--- tests/testthat/test_spartiy_thresholding.R
+++ tests/testthat/test_spartiy_thresholding.R
@@ -5,6 +5,6 @@ test_that("Logging controls work", {
 
     moma_logger_level("INFO")
     expect_equal("INFO", moma_logger_level())
-
+    test_scad(c(1,2.5,4),1,3)
     moma_logger_level("MESSAGE")
 })
