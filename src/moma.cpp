ea9b5f6e2820b41b9055a2f4d33f8d08f96d06c4
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-18 22:44:19 +1000

    Add class MoMA
    - Add MoMA class in `moma.cpp` file, equipped with members.
    - Move proximal operators to `moma_prox.h` file.

--- src/moma.cpp
+++ src/moma.cpp
@@ -1,109 +1,82 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
-#include "RcppArmadillo.h"
-#include "moma_logging.h"
 #include "moma.h"
+#include "moma_prox.h"
 using namespace Rcpp;
 using namespace arma;
-#define MAX(a,b) (a)>(b)?(a):(b)
-#define THRES_P(x,l) (MAX(x-l,0.0)) // takes two doubles, shrink a positive value by `l`
+using namespace std;
+typedef enum { ISTA, FISTA } SOLVER_TYPE;
 
-inline arma::vec soft_thres(const arma::vec &x, double l){
-    return sign(x) % arma::max(abs(x) - l, zeros(size(x)));
+
+/////////////////
+// Section 2: MoMA class
+/////////////////
+std::string str_toupper(std::string s) {
+    std::transform(s.begin(), s.end(), s.begin(), 
+                   [](unsigned char c){ return std::toupper(c); });
+    return s;
 }
 
-class Prox{
+class Grad{  // In our case, we only have grad of such form: f'(x) = Ax + b
 public:
-    virtual arma::vec prox(const arma::vec &x, double l)=0;
+    arma::mat A;    // remains unchanged
+    arma::vec b;    // keeps changing during training
 };
 
-class Lasso: public Prox{
-public:
-    arma::vec prox(const arma::vec &x, double l){
-        return soft_thres(x,l);
-    }
-};
+class MoMA{
 
-class Scad: public Prox{
 private:
-    double gamma; // gamma_SCAD >= 2
+    // Input parameters
+  
+    double prox_u_step; //lambda_u, L_u(S_u(Omeg,alpha_a)
+    double prox_v_step;
+    double grad_u_step;
+    double grad_v_step;
+    
+    SOLVER_TYPE solver_type;
+
+    long MAX_ITER;
+    double EPS;
+
+    // final results
+    arma::vec u; 
+    arma::vec v;
+    // Model, sparse penalty, non_neg
+    Prox *prox_u; 
+    Prox *prox_v;
+    // alpha ==0, S_u = I
+    Grad *grad_u; 
+    Grad *grad_v;
+    arma::mat Su;
+    arma::mat Sv;
+
 public:
-    Scad(double g=3.7){
-        if(g<2) 
-        Rcpp::stop("Gamma for MCP should be larger than 2!\n");
-        gamma=g;
-    }
-    arma::vec prox(const arma::vec &x, double l){
-        int n = x.n_elem;
-        arma::vec z(n);
-        arma::vec absx = arma::abs(x);
-        arma::vec sgn = sign(x);
-        // arma::vec flag = (absx >2);
-        for (int i = 0; i < n; i++) // Probably need vectorization
-        {
-            // the implementation follows Variable Selection via Nonconcave Penalized Likelihood and its Oracle Properties
-            // Jianqing Fan and Runze Li, formula(2.8)
-            z(i) = absx(i) > gamma * l ? absx(i) : (absx(i) > 2 * l ? //(gamma-1)/(gamma-2) * THRES_P(absx(i),gamma*l/(gamma-1)) 
-                                                    ((gamma - 1) * absx(i) - gamma * l)/ (gamma - 2)
-                                                    : THRES_P(absx(i),l)
-                                                    );
-        }
-        return z%sgn;    
-    }
+    // turn user input into what we need to run the algorithm
+    void setup(arma::mat X,
+        // sparsity
+        std::string P_v,
+        std::string P_u, // assume for now they have same type of penalty
+        double lambda_v,
+        double lambda_u,
+        double gamma,
+        bool non_neg, // 1 means activate non-negativity constraint
+        // smoothness
+        arma::mat Omega_u,
+        arma::mat Omega_v,
+        double alpha_u,
+        double alpha_v,
+        // training para.
+        double EPS,
+        long MAX_ITER,
+        std::string solver_type_string
+    ){
+        check_valid();
+        Rcpp::Rcout<< "Converting\n";
+    };
+    void check_valid();
 };
 
 
-class Mcp: public Prox{
-private:
-    double gamma; // gamma_MCP >= 1
-public:
-    Mcp(double g=4){
-        if(g<1) Rcpp::stop("Gamma for MCP should be larger than 1!\n");
-        gamma=g;
-    }
-    arma::vec prox(const arma::vec &x, double l){
-        int n = x.n_elem;
-        arma::vec z(n);
-        arma::vec absx = arma::abs(x);
-        arma::vec sgn = arma::sign(x);
 
-        // arma::vec thr = sgn % arma::max(absx - l, zeros(size(x)));
-        // arma::vec flag = ones<vec>(n) * gamma*l;
-        // arma::vec large = x>flag;
-        // arma::vec small = ones(gamma*l)-large;
-        for (int i = 0; i < n; i++) // Probably need vectorization
-        {
-            // implementation follows lecture notes of Patrick Breheny
-            // http://myweb.uiowa.edu/pbreheny/7600/s16/notes/2-29.pdf
-            // slide 19
-            z(i) = absx(i) > gamma * l ? absx(i)
-                                    : (gamma/(gamma-1)) * THRES_P(absx(i),l);         
-        }
-        return z%sgn;    
-    }
-};
-
-// [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::export]]
-arma::vec prox_lasso(arma::vec x, double l)
-{
-    Lasso a;
-    return a.prox(x,l);
-};
-
-
-// [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::export]]
-arma::vec prox_scad(arma::vec x, double l, double g=3.7)
-{
-    Scad a(g);
-    return a.prox(x,l);
-};
-
-
-// [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::export]]
-arma::vec prox_mcp(arma::vec x, double l, double g=4)
-{
-    Mcp a(g);
-    return a.prox(x,l);
-};
+void MoMA::check_valid(){
+    Rcpp::Rcout << "Checking input validity\n";
+}
