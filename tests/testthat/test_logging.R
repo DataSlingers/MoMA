25a652d2b9868fa406a2db84e2877e9d839e3df6
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-18 16:55:41 +1000

    Fail fixing invalid option "--github_token="
    Revert

--- tests/testthat/test_logging.R
+++ tests/testthat/test_logging.R
@@ -1,3 +1,4 @@
+library(stringr)
 context("Logging Tests")
 
 test_that("Logging controls work", {
@@ -82,7 +83,7 @@ test_that("Function capture works at R level", {
     f <- function(x){MoMA:::moma_error("ERROR MESSAGE")}
 
     e <- tryCatch(f(), error=identity)
-
+    print(e$message)
     expect_str_contains(e$message, "ERROR MESSAGE")
     expect_str_contains(e$message, "(Called from f)")
     expect_true(is.null(e$call))
