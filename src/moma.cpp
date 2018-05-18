79956662b5b01f8ea5e35b790f9aff187324481f
Liao Luofeng <liaoluofeng@192-168-1-166.tpgi.com.au>
2018-05-19 01:03:10 +1000

    Almost finish setup member functin of MoMA class
    - Pass build
    -- Untested.
    
    - Some problems:
    -- Prox * could cause problem due to unreleased memeory. Should be more careful. Need to make sure the default destructor can deal with it.
    -- Find a consistent way of setting default value of `gamma` in SCAD and MCP. Because Lasso doesn't have such parameter. See `string_to_SparseT` function in `moma.cpp`. Also take a look at the constructor of class SCAD and MCP

--- src/moma.cpp
+++ src/moma.cpp
@@ -21,33 +21,62 @@ enum class SparsityType{
 // Section 2: MoMA class
 /////////////////
 
+// // 
+// class Grad{
+// protected:
+//     arma::vec b;  // keeps changing when training
+// public:
+//     Grad() = delete; // has to be initialized
+//     Grad(const arma::vec &b_){b=b_;}
+//     void update(const arma::vec &b_){b=b_;}
+//     virtual arma::vec desc(const arma::vec &x, double stepsize){
+//         return x - stepsize * (x + b);
+//     }
+// };
+
+// class Comm:Grad{    // Common case when A != I
+//     arma::mat A;    // unchanged during training
+// public:
+//     Comm() = delete;
+//     Comm(const arma::vec &b_,const arma::mat &A_){
+//         A = A_;
+//         b = b_;
+//     }
+//     arma::vec desc(const arma::vec &x, double stepsize){
+//         return x - stepsize * (A*x + b);
+//     }
+// };
 
 
 class MoMA{
 
 private:
-    // Input parameters
-    double prox_u_step; //lambda_u, L_u(S_u(Omeg,alpha_a)
+    
+    double prox_u_step; 
     double prox_v_step;
     double grad_u_step;
     double grad_v_step;
     
     Solver solver_type;
-
     long MAX_ITER;
     double EPS;
 
     // final results
     arma::vec u; 
     arma::vec v;
-    // Model, sparse penalty, non_neg
+    // sparse penalty
     Prox *prox_u; // careful about release space and destructor stuff
     Prox *prox_v;
-    // alpha=0, S_u = I
+    // S = I + alpha*Omeg
     arma::mat S_u;  // to be special case
     arma::mat S_v;
     
+    
 public:
+
+    void check_valid();
+    Solver string_to_ST(const std::string &s);
+    Prox* string_to_SparseT(const std::string &s,double gamma);
     // turn user input into what we need to run the algorithm
     MoMA(arma::mat X,
         // sparsity
@@ -65,8 +94,7 @@ public:
         // training para.
         double i_EPS,
         long i_MAX_ITER,
-        std::string i_solver)
-    {
+        std::string i_solver){
     check_valid();
     Rcpp::Rcout<< "Setting up\n";
 
@@ -76,7 +104,7 @@ public:
     EPS = i_EPS;
     solver_type = string_to_ST(i_solver);
 
-    // Step 1: find Su,Sv, and thus Lu,Lv (largest eigenvalue), and thus stepsizes
+    // Step 1: find Su,Sv
     arma::mat U;
     arma::vec s;
     arma::mat V;
@@ -86,9 +114,11 @@ public:
     S_u += n * alpha_u * Omega_u;
     S_v += p * alpha_v * Omega_v;
 
+    // Step 1.2: find Lu,Lv
     double Lu = arma::eig_sym(S_u).max() + 0.01; // +0.01 for convergence
     double Lv = arma::eig_sym(S_v).max() + 0.01;
     
+    // Step 1.3: all kinds of stepsize
     grad_u_step = 1 / Lu;
     grad_v_step = 1 / Lv;
     prox_u_step = lambda_u / Lu;
@@ -98,23 +128,43 @@ public:
     v = V.col(0);
     u = U.col(0);
 
+    // Step 3: match proximal operator
+    prox_u = string_to_SparseT(P_u,gamma);
+    prox_v = string_to_SparseT(P_v,gamma);
+
+    // Step 4: match gradient operator
+    
     };
-    void check_valid();
 
 
-    Solver string_to_ST(const std::string &s){
-        // we can first make s to upper case and provide more flexibility
-        if (s.compare("ISTA") == 0)
-            return Solver::ISTA;
-        else if (s.compare("FISTA") == 0)
-            return Solver::FISTA;
-        else
-            MoMALogger::error("Your choice of algorithm not provided") << s;
-    }
+
 };
 
 
 
 void MoMA::check_valid(){
     Rcpp::Rcout << "Checking input validity\n";
+}
+
+Solver MoMA::string_to_ST(const std::string &s){
+    // we can first make s to upper case and provide more flexibility
+    if (s.compare("ISTA") == 0)
+        return Solver::ISTA;
+    else if (s.compare("FISTA") == 0)
+        return Solver::FISTA;
+    else
+        MoMALogger::error("Your choice of algorithm not provided") << s;
+}
+
+Prox* MoMA::string_to_SparseT(const std::string &s,double gamma){
+    if (s.compare("LASSO") == 0)
+       return new Lasso();
+    else if (s.compare("NONNEGLASSO") == 0)
+        MoMALogger::error("Nonnegative Lasso not implemented yet!\n");
+    else if (s.compare("SCAD") == 0)
+        return new Scad(gamma);
+    else if (s.compare("MCP") == 0)
+        return new Mcp(gamma);
+    else
+        MoMALogger::error("Your sparse penalty is not provided!\n");
 }
