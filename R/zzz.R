b230fed6c24baf52417935bc64fa2840a14385ac
Liao Luofeng <liaoluofeng@LiaodeMacBook-Pro.local>
2018-05-22 23:56:46 +1000

    Fix testthat fail in `test_ISTA`
    - Add `contex("ISTA tests")`

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(...) { # nocov start
     if(interactive()){
-        MoMA::moma_set_logger_level_cpp(0)
+
         msg <- c("Thank you for using MoMA!",
                  "The current logging level is",
                  sQuote(paste0(moma_logger_level(), ".")),
