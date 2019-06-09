36ea141298bcb0f169dc168c4c9c2c8702237ed6
Luofeng Liao <luofengl@student.unimelb.edu.au>
2019-06-09 22:21:47 +0800

    Add BIC_searcher class

---
+++ src/moma_solver_BICsearch.h
@@ -0,0 +1,35 @@
+
+#ifndef MOMA_SOLVER_BICSEARCH_H
+#define MOMA_SOLVER_BICSEARCH_H 1
+
+#include "moma_base.h"
+#include "moma_logging.h"
+#include "moma_solver.h"
+
+class BIC_searcher
+{
+public:
+
+    typedef double(PR_solver:: *Criteria)(arma::vec y, const arma::vec &est);
+    BIC_searcher(){};
+
+    void bind(PR_solver *object, Criteria method);
+
+    double cur_criteria(arma::vec y, const arma::vec &est);
+
+    ~BIC_searcher(){
+        // No need to delete pr_solver
+        MoMALogger::debug("Releasing a BIC_searcher object");
+    }
+
+    Rcpp::List search(const arma::vec &y,   // min_{u} || y - u || + ...penalty...
+                        const arma::vec &u,   // start point
+                        const arma::vec &alpha_u,
+                        const arma::vec &lambda_u);
+
+private:
+    PR_solver *pr_solver;
+    Criteria cri;
+};
+
+#endif
