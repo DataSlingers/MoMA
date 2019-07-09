e2c2e61485732e941e762d50bb445f7edf73188f
Luofeng Liao <luofengl@student.unimelb.edu.au>
2019-07-09 23:09:21 +0800

    R6 SFPCA object

---
+++ R/moma_sfpca.R
@@ -0,0 +1,262 @@
+SFPCA <- R6::R6Class("SFPCA", list(
+    center = NULL,
+    scale = NULL,
+    grid_result = NULL,
+    Omega_u = NULL,
+    Omega_v = NULL,
+    u_sparsity = NULL,
+    v_sparsity = NULL,
+    rank = NULL,
+    alpha_u = NULL,
+    alpha_v = NULL,
+    lambda_u = NULL,
+    lambda_v = NULL,
+    selection_scheme_list = NULL,
+    n = NULL,
+    p = NULL,
+    X = NULL,
+    initialize = function(X, ...,
+                              center = TRUE, scale = FALSE,
+                              u_sparsity = empty(), v_sparsity = empty(), lambda_u = 0, lambda_v = 0, # lambda_u/_v is a vector or scalar
+                              Omega_u = NULL, Omega_v = NULL, alpha_u = 0, alpha_v = 0, # so is alpha_u/_v
+                              pg_setting = moma_pg_settings(),
+                              selection_scheme_str = "gggg",
+                              max_bic_iter = 5,
+                              rank = 1) {
+
+        # Step 1: check ALL arguments
+        # Step 1.1: lambdas and alphas
+        if (!inherits(alpha_u, c("numeric", "integer")) ||
+            !inherits(alpha_v, c("numeric", "integer")) ||
+            !inherits(lambda_u, c("numeric", "integer")) ||
+            !inherits(lambda_v, c("numeric", "integer"))) {
+            moma_error(paste0(
+                "All penalty levels (",
+                sQuote("lambda_u"), ", ",
+                sQuote("lambda_v"), ", ",
+                sQuote("alpha_u"), ", ",
+                sQuote("alpha_v"),
+                ") must be numeric."
+            ))
+        }
+        self$alpha_u <- alpha_u
+        self$alpha_v <- alpha_v
+        self$lambda_u <- lambda_u
+        self$lambda_v <- lambda_v
+
+        # Step 1.2: matrix
+        X <- as.matrix(X)
+        if (any(!is.finite(X))) {
+            moma_error("X must not have NaN, NA, or Inf.")
+        }
+        n <- dim(X)[1]
+        p <- dim(X)[2]
+        X <- scale(X, center = center, scale = scale)
+
+        cen <- attr(X, "scaled:center")
+        sc <- attr(X, "scaled:scale")
+        if (any(sc == 0)) {
+            moma_error("cannot rescale a constant/zero column to unit variance")
+        }
+
+        self$center <- if (is.null(cen)) FALSE else cen
+        self$scale <- if (is.null(sc)) FALSE else sc
+        self$n <- n
+        self$p <- p
+        self$X <- X
+
+        # Step 1.3: sparsity
+        if (!inherits(u_sparsity, "moma_sparsity") || !inherits(v_sparsity, "moma_sparsity")) {
+            moma_error(
+                "Sparse penalty should be of class ",
+                sQuote("moma_sparsity"),
+                ". Try using, for example, `u_sparsity = lasso()`."
+            )
+        }
+        self$u_sparsity <- u_sparsity
+        self$v_sparsity <- v_sparsity
+
+        # Step 1.4: PG loop settings
+        if (!inherits(pg_setting, "moma_pg_settings")) {
+            moma_error(
+                "pg_setting penalty should be of class ",
+                sQuote("moma_pg_settings"),
+                ". Try using, for example, `pg_setting = moma_pg_settings(MAX_ITER=1e+4)`."
+            )
+        }
+
+        # Step 1.5: smoothness
+        Omega_u <- check_omega(Omega_u, alpha_u, n)
+        Omega_v <- check_omega(Omega_v, alpha_v, p)
+        self$Omega_u <- Omega_u
+        self$Omega_v <- Omega_v
+
+        # Step 1.6: check selection scheme string
+        # "g" stands for grid search, "b" stands for BIC
+        if (!inherits(selection_scheme_str, "character") || nchar(selection_scheme_str) != 4 ||
+            !all(strsplit(selection_scheme_str, split = "")[[1]] %in% c("b", "g"))) {
+            moma_error(
+                "Invalid selection_scheme_str ", selection_scheme_str,
+                ". It should be a four-char string containing only 'b' or 'g'."
+            )
+        }
+
+        # turn "b"/"g" to 1/0
+        selection_scheme_list <- list(
+            selection_criterion_alpha_u = 0,
+            selection_criterion_alpha_v = 0,
+            selection_criterion_lambda_u = 0,
+            selection_criterion_lambda_v = 0
+        )
+        for (i in 1:4) {
+            selection_scheme_list[[i]] <- ifelse(strsplit(selection_scheme_str, split = "")[[1]][i] == "g", 0, 1)
+        }
+        self$selection_scheme_list <- selection_scheme_list
+
+        # Step 1.7: check rank
+        if (!inherits(rank, "numeric") || rank <= 0
+        || rank > min(p, n)) {
+            moma_error("rank should be a legit positive integer.")
+        }
+        self$rank <- rank
+
+        # Step 2: pack all arguments in a list
+        algo_settings_list <- c(
+            list(
+                X = X,
+                lambda_u = lambda_u,
+                lambda_v = lambda_v,
+                # smoothness
+                alpha_u = alpha_u,
+                alpha_v = alpha_v,
+                rank = rank
+            ),
+            list(
+                Omega_u = check_omega(Omega_u, alpha_u, n),
+                Omega_v = check_omega(Omega_v, alpha_v, p),
+                prox_arg_list_u = add_default_prox_args(u_sparsity),
+                prox_arg_list_v = add_default_prox_args(v_sparsity)
+            ),
+            pg_setting,
+            selection_scheme_list,
+            list(
+                max_bic_iter = max_bic_iter
+            )
+        )
+        # make sure we explicitly specify ALL arguments
+        if (length(algo_settings_list) != length(formals(cpp_multirank_BIC_grid_search))) {
+            moma_error("Incomplete arguments in SFPCA::initialize.")
+        }
+
+        # Step 3: call the fucntion
+        self$grid_result <- do.call(
+            cpp_multirank_BIC_grid_search,
+            algo_settings_list
+        )
+    },
+
+    get_mat = function(alpha_u = 1, alpha_v = 1, lambda_u = 1, lambda_v = 1) {
+        # Sanity check: if a parameter has been chosen by BIC, then
+        # the index for that parameter should not be specified.
+        parameter <- c(alpha_u, alpha_v, lambda_u, lambda_v)
+        if (any(parameter != 1 & self$selection_scheme_list != 0)) {
+            moma_error("Invalid index in SFPCA::get_mat. Do not specify indexes of parameters chosen by BIC.")
+        }
+
+
+        n <- self$n
+        p <- self$p
+        rank <- self$rank
+
+        U <- matrix(0, nrow = n, ncol = rank)
+        V <- matrix(0, nrow = p, ncol = rank)
+        for (i in (1:self$rank)) {
+            U[, i] <-
+                get_5Dlist_elem(self$grid_result,
+                    alpha_u_i = alpha_u,
+                    lambda_u_i = lambda_u,
+                    alpha_v_i = alpha_v,
+                    lambda_v_i = lambda_v, rank_i = i
+                )[[1]]$u$vector
+
+            V[, i] <-
+                get_5Dlist_elem(self$grid_result,
+                    alpha_u_i = alpha_u,
+                    lambda_u_i = lambda_u,
+                    alpha_v_i = alpha_v,
+                    lambda_v_i = lambda_v, rank_i = i
+                )[[1]]$v$vector
+        }
+
+        coln <- if (is.null(colnames(self$X))) paste0("Xcol_", seq_len(p)) else colnames(self$X)
+        rown <- if (is.null(rownames(self$X))) paste0("Xrow_", seq_len(n)) else rownames(self$X)
+        dimnames(V) <-
+            list(coln, paste0("PC", seq_len(rank)))
+        dimnames(U) <-
+            list(rown, paste0("PC", seq_len(rank)))
+        return(list(U = U, V = V))
+    },
+
+    print = function() {
+        selection_list_str <- lapply(self$selection_scheme_list, function(x) {
+            if (x == 0) {
+                return("grid search")
+            }
+            else if (x == 1) {
+                return("BIC search")
+            }
+        })
+
+        cat("An <SFPCA> object containing solutions to the following settings\n")
+        cat("rank =", self$rank, "\n")
+        cat("Penalty and selection:\n")
+
+        cat(paste0("alpha_u: ", selection_list_str[1]), "\n")
+        print(self$alpha_u)
+        cat(paste0("alpha_u: ", selection_list_str[2]), "\n")
+        print(self$alpha_v)
+        cat(paste0("lambda_u: ", selection_list_str[3]), "\n")
+        print(self$lambda_u)
+        cat(paste0("lambda_v: ", selection_list_str[4]), "\n")
+        print(self$lambda_v)
+    },
+
+    left_project = function(newX, ...,
+                                alpha_u = 1, alpha_v = 1, lambda_u = 1, lambda_v = 1) {
+
+        # check indexes
+        parameter <- c(alpha_u, alpha_v, lambda_u, lambda_v)
+        if (any(parameter != 1 & self$selection_scheme_list != 0)) {
+            moma_error("Invalid index in SFPCA::get_mat. Do not specify indexes of parameters chosen by BIC.")
+        }
+        V <- self$get_mat(
+            alpha_u = alpha_u,
+            alpha_v = alpha_v,
+            lambda_u = lambda_u,
+            lambda_v = lambda_v
+        )$V
+
+
+        # newX should be uncencter and unscaled.
+        # check new X has same colnames
+        if (length(dim(newX)) != 2L) {
+            moma_error("'newX' must be a matrix or data frame")
+        }
+
+        if (dim(newX)[2] != self$p) {
+            moma_error("`newX` is incompatible with orignal data.")
+        }
+
+        PV <- solve((t(V) %*% V), t(V))
+        scaled_data <- scale(newX, self$center, self$scale)
+        result <- scaled_data %*% t(PV)
+        colnames(result) <- paste0("PC", seq_len(self$rank))
+        return(list(
+            scaled_data = scaled_data,
+            proj_data = result,
+            V = V,
+            PV = PV
+        ))
+    }
+))
+
