144b5c0f95e0047e8efe36ed10dcac25e1b034a8
Luofeng Liao <luofengl@student.unimelb.edu.au>
2019-07-19 12:51:18 +0800

    C++ interface for CCA and LDA

--- src/moma_expose.cpp
+++ src/moma_expose.cpp
@@ -205,3 +205,53 @@ Rcpp::List cpp_multirank_BIC_grid_search(
                                 selection_criterion_alpha_v, selection_criterion_lambda_u,
                                 selection_criterion_lambda_v, max_bic_iter, rank);
 }
+
+// [[Rcpp::export]]
+Rcpp::List cca(const arma::mat &X,  // We should not change any variable in R, so const ref
+               const arma::mat &Y,
+               const arma::vec &alpha_u,
+               const arma::vec &alpha_v,
+               const arma::mat &Omega_u,  // Default values for these matrices should be set in R
+               const arma::mat &Omega_v,
+               const arma::vec &lambda_u,
+               const arma::vec &lambda_v,
+               const Rcpp::List &prox_arg_list_u,
+               const Rcpp::List &prox_arg_list_v,
+               double EPS,
+               long MAX_ITER,
+               double EPS_inner,
+               long MAX_ITER_inner,
+               std::string solver,
+               int deflation_scheme,                  // PCA = 1, CCA = 2, LDA = 3, PLS = 4
+               int selection_criterion_alpha_u  = 0,  // 0 means grid, 1 means BIC search
+               int selection_criterion_alpha_v  = 0,
+               int selection_criterion_lambda_u = 0,
+               int selection_criterion_lambda_v = 0,
+               int max_bic_iter                 = 5,
+               int rank                         = 1)
+{
+    int n_lambda_u = lambda_u.n_elem;
+    int n_lambda_v = lambda_v.n_elem;
+    int n_alpha_u  = alpha_u.n_elem;
+    int n_alpha_v  = alpha_v.n_elem;
+
+    if (n_lambda_v == 0 || n_lambda_u == 0 || n_alpha_u == 0 || n_alpha_v == 0)
+    {
+        MoMALogger::error("Please specify all four parameters.");
+    }
+
+    // NOTE: arguments should be listed
+    // in the exact order of MoMA constructor
+    MoMA problem(X, Y,
+                 /* sparsity */
+                 lambda_u(0), lambda_v(0), prox_arg_list_u, prox_arg_list_v,
+                 /* smoothness */
+                 alpha_u(0), alpha_v(0), Omega_u, Omega_v,
+                 /* algorithm parameters */
+                 EPS, MAX_ITER, EPS_inner, MAX_ITER_inner, solver,
+                 static_cast<DeflationScheme>(deflation_scheme));
+
+    return problem.grid_BIC_mix(alpha_u, alpha_v, lambda_u, lambda_v, selection_criterion_alpha_u,
+                                selection_criterion_alpha_v, selection_criterion_lambda_u,
+                                selection_criterion_lambda_v, max_bic_iter, rank);
+}
