6255c889ce5c6844686fdd3e19615f95b9b958aa
Luofeng Liao <luofengl@student.unimelb.edu.au>
2019-07-20 15:58:37 +0800

    Add `is_valid_parameters` and tests

--- R/util.R
+++ R/util.R
@@ -5,3 +5,6 @@
         rhs
     }
 }
+
+is_fin_numeric_scalar  <- function(x) {(length(x) == 1L) && is.numeric(x) && (!is.na(x)) && is.finite(x)}
+is_valid_parameters <- function(x) {length(x) >= 1 && all(sapply(x, is_fin_numeric_scalar))}
---
+++ tests/testthat/test_util.R
@@ -0,0 +1,41 @@
+context("Tests for Util.R")
+
+test_that("is_* functions",{
+
+    expect_true(all(
+        is_fin_numeric_scalar(1),
+        !is_fin_numeric_scalar(c()),
+        is_fin_numeric_scalar(c(1)),
+        !is_fin_numeric_scalar("a"),
+        !is_fin_numeric_scalar(Inf),
+        !is_fin_numeric_scalar(NaN),
+        !is_fin_numeric_scalar(NaN)
+    ))
+
+    expect_true(all(
+        # compatible with `is_fin_numeric_scalar`
+        is_valid_parameters(1),
+        !is_valid_parameters(c()),
+        is_valid_parameters(c(1)),
+        !is_valid_parameters("a"),
+        !is_valid_parameters(Inf),
+        !is_valid_parameters(NaN),
+        !is_valid_parameters(NaN),
+
+        !is_valid_parameters(list()),
+        !is_valid_parameters(c("a",1)),
+        !is_valid_parameters(list(1,list(1,2,3)))
+
+        !is_valid_parameters(c(1,Inf)),
+        !is_valid_parameters(c(Inf,1)),
+        !is_valid_parameters(c(1,NA)),
+        !is_valid_parameters(c(NA)),
+        !is_valid_parameters(c(1,NaN)),
+        !is_valid_parameters(c(NaN,1)),
+
+        is_valid_parameters(seq(0,10)),
+        is_valid_parameters(matrix(c(1,2,3,4),2)),
+        is_valid_parameters(c(1,2,3)),
+        is_valid_parameters(list(1,2,3))
+    ))
+})
