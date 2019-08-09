c77a9b879489e685cb9101c7d9e93bd5089d5e0e
Luofeng Liao <luofengl@student.unimelb.edu.au>
2019-08-09 10:12:46 +0800

    Add doubel square bracket

--- script/linux_install_phantomjs.sh
+++ script/linux_install_phantomjs.sh
@@ -11,9 +11,9 @@ if [[ $OS = Linux ]] ; then
    export PATH=$PWD/travis_phantomjs/phantomjs$PHANTOMJS_VERSIONlinuxx86_64/bin:$PATH
    hash r
    phantomjs version
-   if [ $(phantomjs version) != $PHANTOMJS_VERSION ]; then rm rf $PWD/travis_phantomjs; mkdir p $PWD/travis_phantomjs; fi
-   if [ $(phantomjs version) != $PHANTOMJS_VERSION ]; then wget https://github.com/Medium/phantomjs/releases/download/v$PHANTOMJS_VERSION/phantomjs$PHANTOMJS_VERSIONlinuxx86_64.tar.bz2 O $PWD/travis_phantomjs/phantomjs$PHANTOMJS_VERSIONlinuxx86_64.tar.bz2; fi
-   if [ $(phantomjs version) != $PHANTOMJS_VERSION ]; then tar xvf $PWD/travis_phantomjs/phantomjs$PHANTOMJS_VERSIONlinuxx86_64.tar.bz2 C $PWD/travis_phantomjs; fi
-   if [ $(phantomjs version) != $PHANTOMJS_VERSION ]; then hash r; fi
+   if [[ $(phantomjs version) != $PHANTOMJS_VERSION ]]; then rm rf $PWD/travis_phantomjs; mkdir p $PWD/travis_phantomjs; fi
+   if [[ $(phantomjs version) != $PHANTOMJS_VERSION ]]; then wget https://github.com/Medium/phantomjs/releases/download/v$PHANTOMJS_VERSION/phantomjs$PHANTOMJS_VERSIONlinuxx86_64.tar.bz2 O $PWD/travis_phantomjs/phantomjs$PHANTOMJS_VERSIONlinuxx86_64.tar.bz2; fi
+   if [[ $(phantomjs version) != $PHANTOMJS_VERSION ]]; then tar xvf $PWD/travis_phantomjs/phantomjs$PHANTOMJS_VERSIONlinuxx86_64.tar.bz2 C $PWD/travis_phantomjs; fi
+   if [[ $(phantomjs version) != $PHANTOMJS_VERSION ]]; then hash r; fi
    phantomjs version
 fi
